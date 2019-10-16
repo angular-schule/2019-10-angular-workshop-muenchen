2b25467ec2a7d4bf08b67240b878d026e2aea6c7
Ferdinand Malcher <ferdinand@malcher.media>
2019-10-16 11:21:18 +0200

    Komponententests

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,10 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { Book } from '../shared/book';
 
 describe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
+
+  let book: Book;
+
+  beforeEach(() => {
+    book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3
+    };
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +28,45 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // Wichtig: Daten setzen VOR detectChanges()
+    component.book = book;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event for method call', () => {
+    let emittedBook: Book;
+
+    // Event abonnieren
+    component.rateUp.subscribe(b => {
+      emittedBook = b;
+    });
+
+    // Methode aufrufen
+    component.doRateUp();
+
+    // Prüfen, ob Event ausgelöst
+    expect(emittedBook).toBe(component.book);
+  });
+
+
+  it('should call method for btn click', () => {
+    // Button holen
+    const rateUpBtn: HTMLButtonElement = fixture.nativeElement
+      .querySelector('[data-testid="rateUpBtn"]');
+
+    // Spion überwacht Methode
+    spyOn(component, 'doRateUp');
+
+    // Button klicken
+    rateUpBtn.click();
+
+    // Prüfen, ob Methode aufgerufen
+    expect(component.doRateUp).toHaveBeenCalled();
   });
 });
--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
+import { BookComponent } from '../book/book.component';
 
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  let book: Book;
+  let ratingMock;
+
+  beforeEach(() => {
+    book = {
+      title: '',
+      isbn: '',
+      description: '',
+      rating: 3
+    };
+
+    ratingMock = {
+      rateUp: () => book
+    };
+  });
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent, BookComponent ],
+      providers: [
+        // Abhängigkeit ersetzen: Jeder, der BookRatingService anfordert, erhält ratingMock
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +44,17 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service for rateUp()', () => {
+    const rs = TestBed.get(BookRatingService);
+
+    // Methode überwachen, aber Call trotzdem durchleiten
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    // Methode aufrufen
+    component.rateUp(book);
+
+    expect(rs.rateUp).toHaveBeenCalled();
   });
 });
