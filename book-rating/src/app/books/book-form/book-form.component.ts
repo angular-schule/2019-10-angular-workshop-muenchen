8a246cb27c2b0928073b7249b8187e47e797876a
Ferdinand Malcher <ferdinand@malcher.media>
2019-10-17 10:36:48 +0200

    Reactive Forms: Modell und Validierung

--- book-rating/src/app/books/book-form/book-form.component.ts
+++ book-rating/src/app/books/book-form/book-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { Book } from '../shared/book';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'br-book-form',
@@ -8,16 +9,36 @@ import { Book } from '../shared/book';
 })
 export class BookFormComponent implements OnInit {
 
-  myObj = {
-    title: 'foo',
-    isbn: '123'
-  };
+  bookForm: FormGroup;
 
   @Output() submitBook = new EventEmitter<Book>();
 
   constructor() { }
 
   ngOnInit() {
+    this.bookForm = new FormGroup({
+      isbn: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(13),
+      ]),
+      title: new FormControl('', Validators.required),
+      description: new FormControl(''),
+      price: new FormControl('', [
+        Validators.required,
+        Validators.min(1)
+      ])
+    });
+  }
+
+  isInvalid(name: string) {
+    const control = this.bookForm.get(name);
+    return control.invalid && control.touched;
+  }
+
+  hasError(name: string, errorCode: string) {
+    const control = this.bookForm.get(name);
+    return control.hasError(errorCode) && control.touched;
   }
 
   submitForm() {
